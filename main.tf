c31ae296b8d86250d5016c56e223702a8ad532bb
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2025-08-27 16:14:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "null" {
 
 /* a list of local variables */
 locals {
-  jones = ["indy", "henry", "marian", "shortround", "ok"]
+  jones = ["indy", "henry", "marian", "sallah"]
   trigger_destroy = true
 }
 
